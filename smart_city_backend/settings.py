5c0b722f5011d604ae1d44c42bd7d03fa63ced0f
TAkshayKumar09 <akshaykumart0609@gmail.com>
2026-03-29 17:39:13 +0530

    allowed origins

--- smart_city_backend/settings.py
+++ smart_city_backend/settings.py
@@ -68,7 +68,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "https://your-frontend-url.onrender.com",
     "http://localhost:5173",
-    "https://smart-community-issue-portal.vercel.app/",
+    "https://smart-community-issue-portal.vercel.app",
 ]
 
 
@@ -82,7 +82,7 @@ CORS_ALLOW_METHODS = list(default_methods)
 CSRF_TRUSTED_ORIGINS = [
     "https://your-frontend-url.onrender.com",
     "http://localhost:5173",
-    "https://smart-community-issue-portal.vercel.app/",
+    "https://smart-community-issue-portal.vercel.app",
 ]
 
 # REQUIRED FOR COOKIES (VERY IMPORTANT)
